d885bc8a1dee1f34af78ff0e416842d345ced3cb
otterp012 <otterp012@gmail.com>
2022-06-30 13:06:58 +0900

    [test]: auth삭제

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -6,7 +6,7 @@ import { CookiesProvider } from 'react-cookie';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { AddIssue } from './pages/addIssue';
 import IssueList from './pages/issueList/IssueList';
-import AuthPage from './pages/auth/AuthPage';
+// import AuthPage from './pages/auth/AuthPage';
 import store from './store/store';
 import Callback from './pages/callback/Callback';
 const queryClient = new QueryClient();
@@ -20,7 +20,7 @@ const App = () => {
           <Router>
             <Routes>
               <Route path="/">
-                <Route index element={<AuthPage />} />
+                {/* <Route index element={<AuthPage />} /> */}
                 <Route path="issues" element={<IssueList />} />
                 <Route path="addIssue" element={<AddIssue />} />
                 <Route path="callback" element={<Callback />} />
