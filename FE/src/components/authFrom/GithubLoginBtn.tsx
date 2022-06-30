e531fccf67830eb0dfc5845d08b835024c72ebb8
otterp012 <otterp012@gmail.com>
2022-06-30 15:36:44 +0900

    [fix]: 깃허브로그인 url 수정

--- FE/src/components/authFrom/GithubLoginBtn.tsx
+++ FE/src/components/authFrom/GithubLoginBtn.tsx
@@ -11,7 +11,7 @@ const GithubLoginBtn = () => {
     <div>
       <a
         className={styles.link}
-        href={`https://github.com/login/oauth/authorize?client_id=565469f738966f8bc11a&redirect_uri=http://localhost:3000/callback`}
+        href={`https://github.com/login/oauth/authorize?client_id=565469f738966f8bc11a&redirect_uri=http://3.35.150.211/callback`}
       >
         <div className={styles.github_button}>깃허브로 로그인</div>
       </a>
